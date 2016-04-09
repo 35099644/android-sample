32a52a18a2eb2437bed58b816a5ef19a7fe30358
wwwsec <wwwsec@users.noreply.github.com>
2016-04-09 22:07:53 +0800

    Update MainActivity.java

--- 1/tongxun/Bluetooth/src/com/bluetooth/MainActivity.java
+++ 1/tongxun/Bluetooth/src/com/bluetooth/MainActivity.java
@@ -35,14 +35,12 @@ public class MainActivity extends Activity {
 			switch (arg0.getId()) {
 			
 			case R.id.startServerBtn:
-				//打开服务器
 				Intent serverIntent = new Intent(MainActivity.this, ServerActivity.class);
 				serverIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				startActivity(serverIntent);
 				break;
 				
 			case R.id.startClientBtn:
-				//打开客户端
 				Intent clientIntent = new Intent(MainActivity.this, ClientActivity.class);
 				clientIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				startActivity(clientIntent);
